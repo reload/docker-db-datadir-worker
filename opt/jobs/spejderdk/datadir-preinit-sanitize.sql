f8f41cdf648717954adb0f10a0cf23999c5e6cb4
Mads H. Danquah <mads@danquah.dk>
2017-02-07 10:20:33 +0100

    Have the admin-users password match the username

--- opt/jobs/spejderdk/datadir-preinit-sanitize.sql
+++ opt/jobs/spejderdk/datadir-preinit-sanitize.sql
@@ -1,4 +1,5 @@
--- Set admin password to admin.
-UPDATE `users` SET `pass` = '$S$DnyhybQ1LS.tk1SaVb2M67Fo8Hba/2eSYAmXNlTOwMN0I.ionzAq' WHERE `uid` = 1;
+-- Reset the admin-users password:
+-- Set the reload users password to reload.
+UPDATE `users` SET `pass` = '$S$DhJhihnTzLM3UvzF4klWpYorIt7IJ1vzO9M0Fic1WL7MzNV19Xec' WHERE `uid` = 1 AND `name` = 'reload';
 UPDATE `variable` SET `value` = 's:4:"/tmp";' WHERE `name` = 'file_temporary_path';
 
